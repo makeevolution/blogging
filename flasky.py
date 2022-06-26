6dfb1e3428f4ff2c28ab861714f0ed4697713ec2
makeevolution <aldo_hasibuan@yahoo.com>
2022-06-26 17:50:32 +0200

    try using docker hub instead

--- flasky.py
+++ flasky.py
@@ -6,37 +6,6 @@ import sys
 
 import click
 
-
-# The following import imports from __init__.py of app folder
-from app import create_app, db
-from app.models import Permission, User, Role, Follow, Post
-from app.factories import GenericUser, ModeratorUser, AdminUser
-from flask_migrate import Migrate, upgrade
-from config import config
-
-# Create an instance of an application using a configuration in env var
-usedConfiguration = os.getenv('FLASK_CONFIG') or 'default'
-app = create_app(usedConfiguration)
-# Migrate the existing database, or create a new database if it doesn't exist
-migrate = Migrate(app, db, render_as_batch = True)
-
-@app.cli.command()
-def deploy():
-    """Run deployment"""
-    # Alembic to migrate the new database to latest version
-    upgrade()
-    # Create user roles in the new database
-    Role.insert_roles()
-    
-# Nothing to do with the application, it's here just so that if we run flask shell from cmd, no imports for db, User and Role required
-@app.shell_context_processor
-def make_shell_context():
-    print("Shell started")
-    print(f"WARNING: using database " + \
-            getattr(config[usedConfiguration], "SQLALCHEMY_DATABASE_URI"))
-    return dict(db=db, User=User, Role=Role, Permission = Permission, Follow=Follow, Post=Post, 
-                GenericUser = GenericUser, ModeratorUser = ModeratorUser, AdminUser = AdminUser)
-
 # Coverage output not possible in debug mode, since COV.start() below starts a different
 # thread that actually runs the tests. Check if debug mode is used using sys.gettrace() output.
 
@@ -64,6 +33,28 @@ if (sys.gettrace() is None):
     COV = coverage.coverage(branch = True, include="app/*")
     COV.start()
 
+# The following import imports from __init__.py of app folder
+from app import create_app, db
+from app.models import Permission, User, Role, Follow, Post
+from app.factories import GenericUser, ModeratorUser, AdminUser
+from flask_migrate import Migrate, upgrade
+from config import config
+
+# Create an instance of an application using a configuration in env var
+usedConfiguration = os.getenv('FLASK_CONFIG') or 'default'
+app = create_app(usedConfiguration)
+# Migrate the existing database, or create a new database if it doesn't exist
+migrate = Migrate(app, db, render_as_batch = True)
+
+# Nothing to do with the application, it's here just so that if we run flask shell from cmd, no imports for db, User and Role required
+@app.shell_context_processor
+def make_shell_context():
+    print("Shell started")
+    print(f"WARNING: using database " + \
+            getattr(config[usedConfiguration], "SQLALCHEMY_DATABASE_URI"))
+    return dict(db=db, User=User, Role=Role, Permission = Permission, Follow=Follow, Post=Post, 
+                GenericUser = GenericUser, ModeratorUser = ModeratorUser, AdminUser = AdminUser)
+
 # Configuration for test coverage report. Need to put it here so that it starts scanning
 # before any imports for the app starts.
 COV = None
@@ -83,6 +74,14 @@ def test():
         COV.html_report(directory=covdir)
         print('HTML version: file://%s/index.html' % covdir)
         COV.erase()
+
+@app.cli.command()
+def deploy():
+    """Run deployment"""
+    # Alembic to migrate the new database to latest version
+    upgrade()
+    # Create user roles in the new database
+    Role.insert_roles()
 
 # Create a new database based on current model of db
 # Similar to flask db init, but make our own so we don't depend on that framework!
